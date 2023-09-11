80f7277027752220546a8948497441583d935c8e
Enzo Gómez <enzoeegv@gmail.com>
2023-09-11 16:39:42 -0300

    funciona!!!!!

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-const DATA_URL = 'https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/grupo265';
+const DATA_URL = 'https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/grupo265/';
 
 
-function traerDatos(){
+
+function traerDatos() {
     fetch(DATA_URL)
         .then(response => response.json())
-        .then(data =>{
+        .then(data => {
             console.log(data)
             hacerTable(data);
         })
 }
 
-function borrar(id){
-    fetch(`${DATA_URL}${id}`, {    
-        method: 'DELETE'
+function borrar(id) {
+    fetch(`${DATA_URL}${id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
     })
-    .then(() => traerDatos);
+        .then(() => { traerDatos() });
 
 }
 
-function hacerTable(array){
+function hacerTable(array) {
     let contenedor = document.getElementById('listaDatos');
-    
+    contenedor.innerHTML = ""
     array.forEach(element => {
-    
+
         contenedor.innerHTML += `
         <tr>
             <td>${element.nombre}</td>
             <td>${element.apellido}</td>
             <td>${element.grupo}</td>
             <td>${element.sala}</td>
-            <td><button onclick='borrar(${element._id})' class="btn btn-danger">Delete</td>
+            <td><button id="${element._id}" class="btn btn-danger boton">Delete</td>
         </tr>
         `
     });
 }
 
-document.addEventListener('DOMContentLoaded', () =>{
-    
+document.addEventListener('DOMContentLoaded', () => {
     traerDatos()
+    let tabla = document.getElementById('tablaDatos');
+
+    tabla.addEventListener('click', (e) => {
+        let id = e.target.id;
+        console.log(id);
+        borrar(id)
+    })
 })
